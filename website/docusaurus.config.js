0a69dc04c0022b3c0e7558be001b4ad4b9a86ab0
Liz Argall <liz.argall@gmail.com>
2024-07-20 22:28:49 -0700

    Build website

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,11 +12,11 @@ const config = {
   tagline: 'Problems Solved | Content Created',
   favicon: 'img/favicon.ico',
 
-  // Set the production url of your site here
+  // Set the deploy url of your site here
   url: 'https://lizargall.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/lizargall.github.io/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.

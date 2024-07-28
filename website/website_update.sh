35ad01e694a22e014a542539040f3355756da7a6
Liz Argall <liz.argall@gmail.com>
2024-07-27 21:40:26 -0700

    Testing variables in the script

--- website/website_update.sh
+++ website/website_update.sh
@@ -5,14 +5,15 @@ MESSAGE="Testing variables in the script"
 cd ~/repos/lizargall/ # Go to the build repository 
 git add -v ~/repos/lizargall/
 git commit -m "$MESSAGE"
+git push
 echo \"$MESSAGE\" "pushed to build repo"
-# git push
-# cd website
-# npm run build # Build the site
-# rm -r ~/repos/lizargall.github.io/docs/* # Prepare the deploy repository by deleting all the contents of the docs folder
-# mv build/* ~/repos/lizargall.github.io/docs # Move the contents of the build directory in the build repo to the deploy repo.
-# ls -l ~/repos/lizargall.github.io/docs 
-# cd ~/repos/lizargall.github.io/ 
-# git add -v ~/repos/lizargall.github.io/docs
-# git commit -m $MESSAGE
-# git push
+cd website
+npm run build # Build the site
+rm -r ~/repos/lizargall.github.io/docs/* # Prepare the deploy repository by deleting all the contents of the docs folder
+mv build/* ~/repos/lizargall.github.io/docs # Move the contents of the build directory in the build repo to the deploy repo.
+ls -l ~/repos/lizargall.github.io/docs 
+cd ~/repos/lizargall.github.io/ 
+git add -v ~/repos/lizargall.github.io/docs
+git commit -m $MESSAGE
+git push
+echo \"$MESSAGE\" "pushed to deploy repo"

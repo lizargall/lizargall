23b5e3780823bb811c2b30ccfbb3ed31be160ccb
Liz Argall <liz.argall@gmail.com>
2024-08-01 10:00:55 -0700

    Adding error messages to website_update.sh

--- website/website_update.sh
+++ website/website_update.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-# Script to update docusaurus website 
+Script to update docusaurus website 
 read -p "Commit Message:" MESSAGE 
 cd ~/repos/lizargall/ # Go to the build repository 
 git add -v ~/repos/lizargall/
@@ -8,7 +8,21 @@ git commit -m "$MESSAGE"
 git push
 echo "\"$MESSAGE\" pushed to build repo"
 cd website
-npm run build # Build the site
+
+if [ $? -eq 0 ]; then
+    echo "Site successfully built"
+else
+    echo "Site did not build, use \`npm run start\` to check for error messages"
+    exit 1
+fi
+
+if [ $(ls -A "build/" | wc -l) -eq 2 ]; then
+    echo "Build folder is empty, check that \`npm run start\` built to the correct repository. "
+    exit 1
+else
+    echo "Preparing build repository..."
+fi
+
 rm -r ~/repos/lizargall.github.io/docs/* # Prepare the deploy repository by deleting all the contents of the docs folder
 mv build/* ~/repos/lizargall.github.io/docs # Move the contents of the build directory in the build repo to the deploy repo.
 ls -l ~/repos/lizargall.github.io/docs 
